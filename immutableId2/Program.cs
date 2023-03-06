2b09ad7af006cff34da009955de76872489301a5
michelle beckett <Pellerm@mail.nmc.edu>
2023-03-06 12:23:41 -0500

    Add project files.

---
+++ immutableId2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace immutableID
+{
+    class Student
+    {
+        // auto-implemented properties
+        public int Id { get; init; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // default constructor
+        public Student()
+        {
+            Id = 0;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+
+        public Student(int i)
+        {
+            Id = i;
+            FirstName = string.Empty;
+            LastName = string.Empty;
+        }
+        public Student(int i, string First, string Last)
+        {
+            Id = i;
+            FirstName = First;
+            LastName = Last;
+        }
+
+        public static void Main()
+        {
+            Student student = new(01, "Hazel", "Basil");
+            Console.WriteLine($"Id: {student.Id}\nName: {student.FirstName}{student.LastName}");
+            Student student2 = new(02);
+            student2.FirstName = "Kody";
+            student2.LastName = "Klure";
+            Console.WriteLine($"Id: {student2.Id} \nName: {student2.FirstName}{student2.LastName}");
+        }
+    }
+}
